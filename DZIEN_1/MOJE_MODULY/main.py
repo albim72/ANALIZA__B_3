4969874fb85405d2f616982c1770d28a3b96bdd7
albim72 <marcin.albiniak72@gmail.com>
2024-03-25 11:05:54 +0100

    Create main.py

---
+++ DZIEN_1/MOJE_MODULY/main.py
@@ -0,0 +1,11 @@
+# import dane
+# import dane as dn
+from dane import nb as filia
+from dane import book as bk
+
+#CTRL +D - powielenie linii/bloku tekstu
+#CTRL + / - komentowanie/odkomentowanie zaznaczonych linii kodu
+
+print("_______ czytanie ze źródeł danych - bezpośrednie ___________")
+print(filia)
+print(bk)
