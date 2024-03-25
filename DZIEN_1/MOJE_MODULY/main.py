15464522eab75680ddf86a1de71f592e6498f6c5
albim72 <marcin.albiniak72@gmail.com>
2024-03-25 11:31:26 +0100

    Update main.py

--- DZIEN_1/MOJE_MODULY/main.py
+++ DZIEN_1/MOJE_MODULY/main.py
@@ -2,6 +2,8 @@
 # import dane as dn
 from dane import nb as filia
 from dane import book as bk
+from funkcje.bfunkcje import czytaj_liste,czytaj_slownik
+from klasy.cklasa import CDane
 
 #CTRL +D - powielenie linii/bloku tekstu
 #CTRL + / - komentowanie/odkomentowanie zaznaczonych linii kodu
@@ -9,3 +11,12 @@ from dane import book as bk
 print("_______ czytanie ze źródeł danych - bezpośrednie ___________")
 print(filia)
 print(bk)
+
+print("_______ czytanie pomocą funkcji ___________")
+czytaj_liste(filia)
+czytaj_slownik(bk)
+
+print("_______ czytanie pomocą obiektu ___________")
+cd = CDane(filia,bk)
+cd.czytajl()
+cd.czytajs()
